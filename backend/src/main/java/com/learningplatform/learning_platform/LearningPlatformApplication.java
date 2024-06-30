fc3e2ff8d5fc77d85fc8d8dd14f6fd96ec969d77
rasikasrimal <rasikacooldj620@gmail.com>
2024-07-01 01:08:26 +0530

    Project Initialize

---
+++ backend/src/main/java/com/learningplatform/learning_platform/LearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.learningplatform.learning_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPlatformApplication.class, args);
+	}
+
+}
